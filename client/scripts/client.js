d194558f7b77f262203cfeb013e46134fbc2812e
Dan Zera <dczera@gmail.com>
2017-04-07 23:32:33 -0500

    base client.js file setup with 2 controllers and a factory, connect both to index.html

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,1 +1,17 @@
-console.log('we are here');
+var pmdbApp = angular.module('pmdbApp', []);
+
+pmdbApp.controller('InputController', ['$scope', 'MovieService', function($scope, MovieService) {
+  console.log('InputController loaded');
+  $scope.hello = MovieService.hello;
+}]);
+
+pmdbApp.controller('OutputController', ['$scope', 'MovieService', function($scope, MovieService) {
+  console.log('OutputController loaded');
+  $scope.hello = MovieService.hello;
+}]);
+
+pmdbApp.factory('MovieService', ['$http', function($http) {
+  return {
+    hello: 'hello from the factory'
+  };
+}]);
