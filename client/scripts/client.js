d0766ebb510c571ae699aa9902d80d6256ecc1de
Dan Zera <dczera@gmail.com>
2017-04-08 14:11:55 -0500

    set basic search display, resolve img 404 error with ng-src

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -2,26 +2,35 @@ var pmdbApp = angular.module('pmdbApp', []);
 
 pmdbApp.controller('InputController', ['$scope', 'MovieService', function($scope, MovieService) {
   console.log('InputController loaded');
-  $scope.title = '';
-  $scope.searchOMDB = MovieService.searchOMDB;
+  $scope.title = ''; // data-bound to user input field
+  $scope.searchOMDB = MovieService.searchOMDB; // data-bound to user button click
+  // reference to searchResults object
+  // object contains the OMDB response as a property
   $scope.searchResults = MovieService.searchResults;
-}]);
+  $scope.getPoster = MovieService.getPoster; // data-bound to
+}]); // end 'InputController'
 
 pmdbApp.controller('OutputController', ['$scope', 'MovieService', function($scope, MovieService) {
   console.log('OutputController loaded');
-}]);
+}]); // end 'OutputController'
 
 pmdbApp.factory('MovieService', ['$http', function($http) {
-  // IS THIS THE RIGHT WAY TO GO ABOUT THIS?
+  // searchResults object will be used to store response from the OMDB API
   var searchResults = {};
+
+  // public information
   return {
     // DO WE WANT THE SEARCH RESULTS TO HAVE A REFERENCE STORED TO THE RETURN OBJ?
     searchResults: searchResults, // pass an object referece
     searchOMDB: function(title) {
       $http.get('http://www.omdbapi.com/?t=' + title).then(function(response) {
         console.log(response);
-        searchResults.response = response; // store search response as an object property
-      });
-    }
-  };
-}]);
+        if (response.data.Error) { // alert user if no movie matches search results
+          alert('Movie not found!');
+        } else { // otherwise store response as an object property
+          searchResults.response = response;
+        }
+      }); // end $http.get
+    } // end searchOMDB()
+  }; // end return
+}]); // end 'MovieService'
