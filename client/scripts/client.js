8fb37cd02e1e44e047e659d4dd2dcca17de84342
Dan Zera <dczera@gmail.com>
2017-04-08 00:05:29 -0500

    add searchOMDB function to movie service - confirm connection to OMDB API

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -2,16 +2,20 @@ var pmdbApp = angular.module('pmdbApp', []);
 
 pmdbApp.controller('InputController', ['$scope', 'MovieService', function($scope, MovieService) {
   console.log('InputController loaded');
-  $scope.hello = MovieService.hello;
+  $scope.title = '';
+  MovieService.searchOMDB($scope.title);
 }]);
 
 pmdbApp.controller('OutputController', ['$scope', 'MovieService', function($scope, MovieService) {
   console.log('OutputController loaded');
-  $scope.hello = MovieService.hello;
 }]);
 
 pmdbApp.factory('MovieService', ['$http', function($http) {
   return {
-    hello: 'hello from the factory'
+    searchOMDB: function(title) {
+      $http.get('http://www.omdbapi.com/?t=Galaxy+Quest').then(function(response) {
+        console.log(response);
+      });
+    }
   };
 }]);
